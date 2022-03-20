a563964144d1b74431ffc292d841ef262e92a815
Kemal <ttnckemal@gmail.com>
2022-03-20 16:35:06 +0100

    commit

--- Structure/app.js
+++
@@ -1,9 +0,0 @@
-// express işlemleri
-const express = require('express')
-const routes = require('./routes/routes')
-const app = express()
-
-app.use(express.json())
-app.use('/api/v1/tours', routes)
-
-module.exports = app
--- Structure/index.js
+++
@@ -1,9 +0,0 @@
-//server kurulumu 
-
-const app = require('./app')
-
-const PORT = process.env.PORT || 8001
-
-app.listen(PORT, (req, res) => {
-    console.log(`App listening on port ${PORT}`);
-})
